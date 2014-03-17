8f4ea77a24ab21c0cdfbfd4a6069f4c5d99c55e9
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2014-03-17 18:14:20 +0100

    cleanup: Removed unnecessary ModelViewerTestApp class

--- src/main/java/org/flightclub/ModelViewerTestFrame.java
+++ src/main/java/org/flightclub/ModelViewerTestFrame.java
@@ -4,9 +4,7 @@ public class ModelViewerTestFrame extends ModelFrame {
     public ModelViewerTestFrame() {
         super(new ModelViewerTest(), "Model Viewer Test", 550, 350);
     }
-}
 
-class ModelViewerTestApp {
     public static void main(String s[]) {
         ModelViewerTestFrame f = new ModelViewerTestFrame();
     }
