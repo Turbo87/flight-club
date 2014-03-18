77011601b77b8d9c5fe351043811189d500bab12
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2014-03-19 00:46:22 +0100

    FlyingDot: Simplified avoidHills() and setLocalFrame() methods

--- src/main/java/org/flightclub/FlyingDot.java
+++ src/main/java/org/flightclub/FlyingDot.java
@@ -115,34 +115,27 @@ public class FlyingDot implements ClockObserver, CameraSubject {
         Tools3d.cross(v, new Vector3d(0, 0, 1), axisX);
         axisX.scaleBy(1 / axisX.length());
 
-        axisY.set(v);
-        axisY.scaleBy(1 / axisY.length());
+        axisY.set(v).scaleBy(1 / axisY.length());
 
         Tools3d.cross(axisX, axisY, axisZ);
 
         //now apply roll, if any
-        if (roll == 0) return;
+        if (roll == 0)
+            return;
 
         Vector3d up = axisZs[roll + ROLL_STEPS];
 
-        Vector3d axisX0 = new Vector3d();
-        Vector3d axisZ0 = new Vector3d();
-        Vector3d dx = new Vector3d();
-        Vector3d dz = new Vector3d();
+        Vector3d axisX0 = new Vector3d(axisX);
+        Vector3d axisZ0 = new Vector3d(axisZ);
 
-        axisX0.set(axisX);
-        axisZ0.set(axisZ);
+        Vector3d dx = new Vector3d(axisX0).scaleBy(up.x);
+        Vector3d dz = new Vector3d(axisZ0).scaleBy(up.z);
 
-        dx.set(axisX0);
-        dz.set(axisZ0);
-        dx.scaleBy(up.x);
-        dz.scaleBy(up.z);
         axisZ.set(dx).add(dz);
 
-        dx.set(axisX0);
-        dz.set(axisZ0);
-        dx.scaleBy(up.z);
-        dz.scaleBy(-up.x);
+        dx.set(axisX0).scaleBy(up.z);
+        dz.set(axisZ0).scaleBy(-up.x);
+
         axisX.set(dx).add(dz);
     }
 
