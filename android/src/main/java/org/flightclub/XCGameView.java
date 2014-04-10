87f3baf732a141153d9bf57a120308e37909b0ca
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2014-04-10 18:08:25 +0200

    Android: Added XCGameView class

---
+++ android/src/main/java/org/flightclub/XCGameView.java
@@ -0,0 +1,19 @@
+package org.flightclub;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class XCGameView extends View {
+    public XCGameView(Context context) {
+        this(context, null);
+    }
+
+    public XCGameView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public XCGameView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
