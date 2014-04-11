cda43b7a196b9551a9da961f7b934f2a8110823b
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2014-04-11 21:25:25 +0200

    Android: Connect XCGame and XCGameView with AndroidGraphics

--- android/src/main/java/org/flightclub/XCGameView.java
+++ android/src/main/java/org/flightclub/XCGameView.java
@@ -1,10 +1,19 @@
 package org.flightclub;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.view.View;
 
+import org.flightclub.graphics.android.AndroidGraphics;
+
 public class XCGameView extends View {
+    public static final int WIDTH = 400;
+    public static final int HEIGHT = 720;
+
+    private final XCGame app = new XCGame();
+    private final AndroidGraphics graphics = new AndroidGraphics();
+
     public XCGameView(Context context) {
         this(context, null);
     }
@@ -15,5 +24,30 @@ public class XCGameView extends View {
 
     public XCGameView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+
+        app.init(new Interface() {
+            @Override
+            public int getWidth() {
+                return WIDTH;
+            }
+
+            @Override
+            public int getHeight() {
+                return HEIGHT;
+            }
+
+            @Override
+            public void play(String s) {
+
+            }
+        });
+        app.start();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        graphics.setCanvas(canvas);
+        app.draw(graphics, WIDTH, HEIGHT);
+        invalidate();
     }
 }
