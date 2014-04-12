99ae268abbf458039ed185c2c5e21c6cc1b65347
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2014-04-12 16:25:54 +0200

    XCGame: Added environmental lighting
    see http://blog.xoppa.com/basic-3d-using-libgdx-2/

--- core/src/org/flightclub/XCGame.java
+++ core/src/org/flightclub/XCGame.java
@@ -5,16 +5,19 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.g3d.Environment;
 import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 
 import static com.badlogic.gdx.graphics.VertexAttributes.Usage;
 
 public class XCGame extends ApplicationAdapter {
+    public Environment environment;
     public PerspectiveCamera cam;
     public ModelBatch modelBatch;
     public Model model;
@@ -22,6 +25,10 @@ public class XCGame extends ApplicationAdapter {
 
     @Override
 	public void create () {
+        environment = new Environment();
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+        environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+
         modelBatch = new ModelBatch();
 
         cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
@@ -44,7 +51,7 @@ public class XCGame extends ApplicationAdapter {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
         modelBatch.begin(cam);
-        modelBatch.render(instance);
+        modelBatch.render(instance, environment);
         modelBatch.end();
     }
 
