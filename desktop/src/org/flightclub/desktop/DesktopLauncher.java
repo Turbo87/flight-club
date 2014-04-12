0290dc53dfc96b56ec4935c9df4affcdfe592ba1
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2014-04-12 14:55:00 +0200

    Generated new libgdx project

---
+++ desktop/src/org/flightclub/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.flightclub.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.flightclub.XCGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new XCGame(), config);
+	}
+}
